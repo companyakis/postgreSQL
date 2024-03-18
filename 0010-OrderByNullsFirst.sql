da23c02f6c47e955378b9f2f5d569df6a011b008
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 13:02:47 +0300

    Create 0010-OrderByNullsFirst.sql

---
+++ 0010-OrderByNullsFirst.sql
@@ -0,0 +1,18 @@
+SELECT employee_id, first_name, last_name, phone_number
+
+FROM employees
+
+ORDER BY phone_number NULLS FIRST;
+
+/*
+29	"Karen"	"Partners"	
+20	"Jack"	"Livingston"	
+31	"Charles"	"Johnson"	
+19	"Jonathon"	"Taylor"	
+28	"John"	"Russell"	
+21	"Kimberely"	"Grant"	
+37	"Jennifer"	"Whalen"	"515.123.4444"
+1	"Steven"	"King"	"515.123.4567"
+2	"Neena"	"Kochhar"	"515.123.4568"
+3	"Lex"	"De Haan"	"515.123.4569"
+*/
