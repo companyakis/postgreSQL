b271e13a5cccacf262df7e4f5dd5b1e3e8f0b9ab
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 10:34:02 +0300

    Create 0005-Exercise2.sql

---
+++ 0005-Exercise2.sql
@@ -0,0 +1,9 @@
+--Office Data
+
+--Salary increase (%50)
+
+SELECT first_name, last_name, first_name, salary, salary * 1.5 AS new_salary
+
+FROM employees;
+
+--"Steven"	"King"	"Steven"	24000.00	36000.000
