78780058790af0b70a878bc9f1ab71ef27a15e08
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 10:30:45 +0300

    Create 0005-Exercise1.sql

---
+++ 0005-Exercise1.sql
@@ -0,0 +1,11 @@
+--Office Data
+
+--I'm hiring these employees:)
+
+SELECT first_name, last_name, first_name || '.' || last_name || '@companyofmustafabuyukdereli.net'
+
+FROM employees;
+
+-- "Steven" 
+-- "King"
+-- "Steven.King@companyofmustafabuyukdereli.net"
