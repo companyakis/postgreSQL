4f0ecc8ae7ea3a2317632cac7d7b513e215f9502
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-18 13:04:40 +0300

    Create 0011-OrderByNullsLast.sql

---
+++ 0011-OrderByNullsLast.sql
@@ -0,0 +1,17 @@
+SELECT employee_id, phone_number
+
+FROM employees
+
+ORDER BY phone_number NULLS LAST;
+
+/*
+18	"650.124.1224"
+32	"650.501.1876"
+33	"650.501.2876"
+29	
+28	
+31	
+20	
+19	
+21	
+*/
