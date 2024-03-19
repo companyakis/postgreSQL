b8ac947baf540c8d8b20c1b2040307d75ff61d13
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-19 10:36:55 +0300

    Create 0013-Where.sql

---
+++ 0013-Where.sql
@@ -0,0 +1,24 @@
+SELECT film_id, title, release_year 
+
+FROM film
+
+WHERE release_year <> 2005
+
+ORDER BY film_id;
+
+/*
+
+first 10 rows:
+
+1	"Academy Dinosaur"	2006
+2	"Ace Goldfinger"	2006
+3	"Adaptation Holes"	2006
+4	"Affair Prejudice"	2006
+5	"African Egg"	2006
+6	"Agent Truman"	2006
+7	"Airplane Sierra"	2006
+8	"Airport Pollock"	2006
+9	"Alabama Devil"	2006
+10	"Aladdin Calendar"	2006
+
+*/
