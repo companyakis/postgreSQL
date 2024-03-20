3c1820c3e7922de8c49eea10232ac9284cd6d331
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 10:20:41 +0300

    Create 0016-In.sql

---
+++ 0016-In.sql
@@ -0,0 +1,14 @@
+SELECT * FROM customer
+
+WHERE first_name IN ('Jamie', 'Willie')
+
+ORDER BY first_name ASC;
+
+/*
+
+146	1	"Jamie"	"Rice"	"jamie.rice@sakilacustomer.org"	150	true	"2006-02-14"	"2013-05-26 14:49:45.738"	1
+531	2	"Jamie"	"Waugh"	"jamie.waugh@sakilacustomer.org"	537	true	"2006-02-14"	"2013-05-26 14:49:45.738"	1
+219	2	"Willie"	"Howell"	"willie.howell@sakilacustomer.org"	223	true	"2006-02-14"	"2013-05-26 14:49:45.738"	1
+359	2	"Willie"	"Markham"	"willie.markham@sakilacustomer.org"	364	true	"2006-02-14"	"2013-05-26 14:49:45.738"	1
+
+*/
