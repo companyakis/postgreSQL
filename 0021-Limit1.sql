4e4f74f8bcc5cee28145a528af77533e42c39774
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-02 13:08:15 +0300

    Create 0021-Limit1.sql

---
+++ 0021-Limit1.sql
@@ -0,0 +1,15 @@
+SELECT * 
+
+FROM employees
+
+ORDER BY first_name ASC
+
+LIMIT 5;
+
+/*
+25	"Adam"	"Fripp"	"adam.fripp@sqltutorial.org"	"650.123.2234"	"2018-04-10"	19	11808.00	1	5
+13	"Alexander"	"Khoo"	"alexander.khoo@sqltutorial.org"	"515.127.4562"	"2016-05-18"	13	3100.00	23	3
+4	"Alexander"	"Hunold"	"alexander.hunold@sqltutorial.org"	"590.423.4567"	"2011-01-03"	9	9000.00	3	6
+33	"Britney"	"Everett"	"britney.everett@sqltutorial.org"	"650.501.2876"	"2018-03-03"	17	3900.00	27	5
+5	"Bruce"	"Ernst"	"bruce.ernst@sqltutorial.org"	"590.423.4568"	"2012-05-21"	9	6000.00	4	6
+*/
